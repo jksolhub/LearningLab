7e71122acf2932ce5d822f200eccb32af21201b0
KENNEDY <joseph.kennedy@apl.sg>
2019-03-28 14:42:49 +0800

    Updated with Sample Wordcount code

--- ProjectFiles/WCount/settings.py
+++ ProjectFiles/WCount/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'WCount.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
